e8f52b284fb6e0ef3302a21d03efaf4e0e13223e
Angeela <acharya.angeela03@gmail.com>
2021-04-25 21:23:42 -0400

    Changes

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -9,21 +9,22 @@ import { Student } from './student';
   providedIn: 'root'
 })
 export class StudentService {
-  private baseUrl1 = 'http://a6b85c10de9f54b0dac932cf5a2a7f29-796556045.us-east-1.elb.amazonaws.com:9191/students';
-
-  private baseUrl2 = 'http://a6b85c10de9f54b0dac932cf5a2a7f29-796556045.us-east-1.elb.amazonaws.com:9191/addStudent';
+  // private baseUrl1 = 'http://a6b85c10de9f54b0dac932cf5a2a7f29-796556045.us-east-1.elb.amazonaws.com:9191/students';
+  // private baseUrl2 = 'http://a6b85c10de9f54b0dac932cf5a2a7f29-796556045.us-east-1.elb.amazonaws.com:9191/addStudent';
+  private baseUrl1 = 'http://a8727104ed448441ca45544dbdcab647-250204293.us-east-1.elb.amazonaws.com:8081/kafka/students';
+  private baseUrl2 ='http://a8727104ed448441ca45544dbdcab647-250204293.us-east-1.elb.amazonaws.com:8081/kafka/addStudent';
   //https://cors-anywhere.herokuapp.com/
   constructor(private http: HttpClient) { }
 
 
   
   getStudent(): Observable<Student[]> {
-    return this.http.get<Student[]>(this.baseUrl1);
+    return this.http.get<Student[]>(this.baseUrl1).pipe(catchError(this.handleError))
   }
 
 
   public postStudent(student: object) {
-    return this.http.post(this.baseUrl2, student).pipe(catchError(this.handleError))
+    return this.http.post(this.baseUrl2, student, {responseType: 'text' }).pipe(catchError(this.handleError))
   }
 
   private handleError(error: HttpErrorResponse) {
@@ -31,7 +32,7 @@ export class StudentService {
       console.error('An error occurred:', error.error.message);
     } else {
       console.error(
-        `Backend error occured  ${error.status}, ` );
+        'Backend error occured:', error);
     }
     return throwError(
       'unknown error occured');
